3207386dc33ff1ccd1d0d776e8e6a0108f53ea0f
Matt Waelder <mattwaelder@gmail.com>
2022-09-19 17:29:50 -0700

    nearly have the photo/review endpoint returning correct data, my query is a bit off though and its hard to tell how to fix it

--- server/index.js
+++ server/index.js
@@ -45,9 +45,6 @@ console.log(`Listening at http://localhost:${process.env.PORT}`);
 //reviews does not have a link to characteristics or review_photos, ill need to make one
 //~~actually only for photos, characteristics are sepperated into their own api call...
 
-//set up link between an individual review and its photos
-//query returning all entire review with one photo (for as many times as there are photos) rather than returning one review with many photos...
-
-//set up getReviewsById to transform response data in the controller (ideally, maybe model for ease) for things like count, sort, etc...
-
 //set up reviews/product_id/meta to return obj with reviews.ratings info and characteristic info with names instead of numbers
+
+//believe im only returning one review at a time, not the full list
--- server/model.js
+++ server/model.js
@@ -1,18 +1,24 @@
 // import db from "./db.js";
 const db = require("./db.js");
-//going to have queries for any operation
-
-// module.exports.getAllReviews = (data, res) => {
-//   console.log("model received request");
-//   db.query(`SELECT * FROM reviews WHERE product_id = ${data}`)
-//     .then((data) => res.send(data.rows))
-//     .catch((err) => console.log(err));
-// };
 
 module.exports.getAllReviews = (data, res) => {
-  console.log("model received request");
+  console.log("model received request for reviews");
+  // db.query(
+  //   `SELECT (json_agg(review_photos)) FROM review_photos WHERE review_photos.review_id = 5;`
+  // ).then((data) => res.send(data.rows[0].json_agg));
+
+  //prob with subquery where im linking to product id not review id
+  //or, if review id is right im not returning all reviews for that product
+  //i think my subquery needs a subquery...
+  //or maybe this entire query becomes a subquery...
   db.query(
-    `SELECT reviews.*, review_photos.url AS photos FROM reviews, review_photos WHERE reviews.review_id = review_photos.review_id AND reviews.review_id = 5;`
+    `SELECT * FROM reviews,
+      (SELECT (json_agg(review_photos))
+      AS photos
+      FROM review_photos
+      WHERE review_photos.review_id = 5)
+    AS photos
+    WHERE reviews.product_id = 2;`
   )
     .then((data) => res.send(data.rows))
     .catch((err) => console.log(err));
@@ -22,7 +28,7 @@ module.exports.getMetaById = (data, res) => {
   console.log("model received meta request for: ", data);
   //data===product_id
   db.query(`SELECT rating FROM reviews WHERE product_i = ${data}`)
-    .then((data) => res.send(data.rows))
+    .then((data) => res.send(data))
     .catch((err) => console.log(err.stack));
 };
 
