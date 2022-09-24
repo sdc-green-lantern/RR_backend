c28930059d1d1305086aa4f09d716b71fdb697fb
Matt Waelder <mattwaelder@gmail.com>
2022-09-24 14:45:02 -0700

    ammended sql file to use relative pathing

--- server/k6.js
+++ server/k6.js
@@ -7,9 +7,9 @@ export const options = {
   vus: 10,
   stages: [
     { duration: "1m", target: 50 },
-    { duration: "1m", target: 100 },
+    { duration: "1m", target: 200 },
     { duration: "1m", target: 300 },
-    { duration: "1m", target: 150 },
+    { duration: "1m", target: 500 },
     { duration: "1m", target: 0 },
   ],
   thresholds: {
